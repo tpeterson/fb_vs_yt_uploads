f2595f09da18548fffb03d8b6d82e8e32656d534
Tim <tpeterson@users.noreply.github.com>
2016-08-29 20:16:03 -0700

    Add files to get more than default number of videos aka 25 videos from Facebook and from YouTube

---
+++ facebook/get_channelVidsALT.js
@@ -0,0 +1,55 @@
+const https = require('https');
+const url = require('url');
+
+const token = process.env.FACEBOOK_GRAPH;
+const vids_to_get = 100;
+let total_vids = [];
+
+function fetchPageVideos(channel_ID) {
+  let q = {
+    access_token: token
+  };
+
+  let link = {
+    protocol: 'https',
+    hostname: 'graph.facebook.com/v2.7',
+    pathname: channel_ID + '/videos',
+    query: q
+  };
+
+  return new Promise(function(resolve, reject) {
+    reqVids(url.format(link), resolve);
+  });
+}
+
+function reqVids(address, cb) {
+  https.request(address, function(res) {
+    let data = new Buffer(0);
+
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function() {
+      let info = JSON.parse(data.toString());
+      let videos = info.data;
+      total_vids = total_vids.concat(videos);
+      if (info.paging.next && (total_vids.length < vids_to_get)) {
+        reqVids(info.paging.next, cb);
+      } else {
+        cb(total_vids);
+      }
+    });
+  }).end();
+}
+
+
+if (!module.parent) {
+  let channel_ID = '1318800798260799';
+  fetchPageVideos(channel_ID).then(function(res) {
+    console.log(res.length);
+    console.log(res[0] ? res[0] : 'no videos');
+  });
+}
+
+module.exports = fetchPageVideos;
---
+++ youtube/get_channelVidsALT.js
@@ -0,0 +1,114 @@
+const https = require('https');
+const url = require('url');
+
+const token = process.env.YOUTUBE;
+const vids_to_get = 100;
+let total_vids = [];
+
+function fetchRecentUploadsID(channel_name) {
+  let q = {
+    part: 'contentDetails',
+    forUsername: channel_name,
+    key: token
+  };
+
+  let link = {
+    protocol: 'https',
+    hostname: 'www.googleapis.com/youtube/v3',
+    pathname: 'channels',
+    query: q
+  };
+
+  return new Promise(function(resolve, reject) {
+    https.request(url.format(link), function(res) {
+      let data = new Buffer(0);
+
+      res.on('data', function(chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function() {
+        let info = JSON.parse(data.toString());
+        resolve(info);
+      })
+    }).end();
+  });
+}
+
+function fetchChannelVideos(channel_ID) {
+  let q = {
+    part: 'snippet',
+    playlistId: channel_ID,
+    maxResults: 25,
+    key: token
+  };
+
+  let link = {
+    protocol: 'https',
+    hostname: 'www.googleapis.com/youtube/v3',
+    pathname: 'playlistItems',
+    query: q
+  };
+
+  return new Promise(function(resolve, reject) {
+    reqVids(url.format(link), channel_ID, resolve);
+  });
+}
+
+function reqVids(address, channel, cb) {
+  https.request(address, function(res) {
+    let data = new Buffer(0);
+
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function() {
+      let info = JSON.parse(data.toString());
+      let videos = info.items;
+      total_vids = total_vids.concat(videos);
+      if (info.nextPageToken && (total_vids.length < vids_to_get)) {
+        let q = {
+          part: 'snippet',
+          playlistId: channel,
+          maxResults: 25,
+          nextPageToken: info.nextPageToken,
+          key: token
+        };
+
+        let link = {
+          protocol: 'https',
+          hostname: 'www.googleapis.com/youtube/v3',
+          pathname: 'playlistItems',
+          query: q
+        };
+
+        let new_address = url.format(link);
+        reqVids(new_address, channel, cb);
+      } else {
+        cb(total_vids);
+      }
+    });
+  }).end();
+}
+
+function fetchRecentUploadsList(channel_name) {
+  return fetchRecentUploadsID(channel_name).then(function(res) {
+    let uploads_id = res.items[0].contentDetails.relatedPlaylists.uploads;
+    console.log(uploads_id);
+
+    return fetchChannelVideos(uploads_id).then(function(res) {
+      return res;
+    });
+  });
+}
+
+if (!module.parent) {
+  let channel_name = 'BuzzFeedVideo';
+  fetchRecentUploadsList(channel_name).then(function(video_list) {
+    console.log(video_list.length);
+    console.log(video_list[0] ? video_list[0] : 'no videos');
+  });
+}
+
+module.exports = fetchRecentUploadsList;
