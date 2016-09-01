670ec1fcd138a2b3dfb482d2223c9e962a45dfdc
Tim <tpeterson@users.noreply.github.com>
2016-09-01 13:23:54 -0700

    Add script to calculate how many videos a publisher is posting to each platform per day on average

---
+++ calc_AvgUploadsByDay.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const processHelper = require('./processHelper');
+
+const publisher = 'tastemade'
+
+const fb_data = fs.readFileSync(`./data/vid_lists/facebook/${publisher}vids_200.json`);
+const yt_data = fs.readFileSync(`./data/vid_lists/youtube/${publisher}vids_200.json`);
+
+const fb_vids = JSON.parse(fb_data);
+const yt_vids = JSON.parse(yt_data);
+
+let fb_vids_byDate = processHelper.addVidsByDate(fb_vids, 'Facebook');
+let yt_vids_byDate = processHelper.addVidsByDate(yt_vids, 'YouTube');
+
+function getAvgVidsPerDay(date_arr, vid_arr) {
+  let total_vids = vid_arr.length;
+  let total_days = date_arr.length;
+
+  return total_vids / total_days;
+}
+
+let fb_avg = getAvgVidsPerDay(fb_vids_byDate, fb_vids);
+let yt_avg = getAvgVidsPerDay(yt_vids_byDate, yt_vids);
+
+console.log(`Facebook: ${fb_avg} videos per day`);
+console.log(`YouTube: ${yt_avg} videos per day`);
