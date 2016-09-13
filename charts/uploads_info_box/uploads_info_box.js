558d12c7a786ea74c2bb674461fff5e21b601edc
Tim <tpeterson@users.noreply.github.com>
2016-09-13 16:50:34 -0700

    Initial version of publisher-specific info chart that displays info category for each box and displays actual info once clicked or touched

---
+++ charts/uploads_info_box/uploads_info_box.js
@@ -0,0 +1,23 @@
+(function() {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var boxes = document.getElementsByClassName('box_content');
+    Array.prototype.forEach.call(boxes, function(box) {
+      box.addEventListener('click', showInfo);
+      box.addEventListener('touchstart', showInfo);
+
+    });
+  });
+
+  function showInfo(e) {
+    e.preventDefault();
+    var el_children = this.children;
+
+    if (el_children[1].style.visibility !== 'visible') {
+      el_children[0].style.visibility = 'hidden';
+      el_children[1].style.visibility = 'visible';
+    } else {
+      el_children[1].style.visibility = 'hidden';
+      el_children[0].style.visibility = 'visible';
+    }
+  }
+})();
